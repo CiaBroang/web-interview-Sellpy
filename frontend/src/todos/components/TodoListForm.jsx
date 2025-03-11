0d0932e63599a0e192bcccb6e86dc6d34add0a56
Cia Broang <cia.broang@sj.se>
2025-03-11 22:38:42 +0100

    Clean up

--- frontend/src/todos/components/TodoListForm.jsx
+++ frontend/src/todos/components/TodoListForm.jsx
@@ -11,16 +11,12 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
 
 export const TodoListForm = ({ todoList, saveTodoList }) => {
-  const todos = todoList.todos //UseState? setTodos(todosToSubmit) i min handleSubmit
+  const todos = todoList.todos
 
   const handleSubmit = async (todosToSubmit) => {
     const listId = todoList.id
 
     const data = { listId, todos: todosToSubmit }
-
-    // Jag skulle vilja validera data innan det skickas till backend
-    // Kunna visa upp fel på specifika fält?
-    console.log('Submitting data to server:', data)
 
     const response = await fetch('http://localhost:3001/data', {
       method: 'POST',
@@ -45,7 +41,6 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
     const updatedTodos = todos.filter((todo) => todo.id !== todoId)
 
     saveTodoList(listId, { todos: updatedTodos })
-    console.log('New todos efter delete', todos)
   }
 
   return (
@@ -53,63 +48,59 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
       <CardContent>
         <Typography component='h2'>{todoList.title}</Typography>
         <form style={{ display: 'flex', flexDirection: 'column', flexGrow: 1, gap: '1rem' }}>
-          {todos.map(
-            (todoToRender, index) =>
-              console.log('todo', todoToRender) || (
-                <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography sx={{ margin: '8px' }} variant='h6'>
-                    {index + 1}
-                  </Typography>
-                  <Checkbox
-                    checked={todoToRender.completed}
-                    onChange={(event) => {
-                      const updatedTodos = todos.map((todo) =>
-                        todoToRender.id === todo.id
-                          ? { ...todo, completed: event.target.checked }
-                          : todo
-                      )
-                      console.log('todo with date', todoToRender)
-                      handleSubmit(updatedTodos)
-                    }}
-                  />
-                  <TextField
-                    sx={{ flexGrow: 1, marginTop: '1rem', marginRight: '1rem' }}
-                    label='What to do?'
-                    value={todoToRender.title}
-                    onChange={(event) => {
-                      const updatedTodos = todos.map((todo) =>
-                        todoToRender.id === todo.id ? { ...todo, title: event.target.value } : todo
-                      )
-                      handleSubmit(updatedTodos) //Antagligen inte så bra med så många anrop, kolla timer
-                    }}
-                  />
-                  <TextField
-                    type='date'
-                    sx={{ flexGrow: 1, marginTop: '1rem' }}
-                    value={todoToRender.dueDate ? todoToRender.dueDate.split('T')[0] : ''}
-                    onChange={(event) => {
-                      const updatedTodos = todos.map((todo) =>
-                        todoToRender.id === todo.id //lite osäkert med index här kanske, borde nog använda ID och isf byta namn på todo inre/yttre scope
-                          ? {
-                              ...todo,
-                              dueDate: new Date(event.target.value).toISOString(),
-                            }
-                          : todo
-                      )
-                      handleSubmit(updatedTodos)
-                    }}
-                  />
-                  <Button
-                    sx={{ margin: '8px' }}
-                    size='small'
-                    color='primary'
-                    onClick={() => handleDelete(todoList.id, todoToRender.id)}
-                  >
-                    <DeleteIcon />
-                  </Button>
-                </div>
-              )
-          )}
+          {todos.map((todoToRender, index) => (
+            <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
+              <Typography sx={{ margin: '8px' }} variant='h6'>
+                {index + 1}
+              </Typography>
+              <Checkbox
+                checked={todoToRender.completed}
+                onChange={(event) => {
+                  const updatedTodos = todos.map((todo) =>
+                    todoToRender.id === todo.id
+                      ? { ...todo, completed: event.target.checked }
+                      : todo
+                  )
+                  handleSubmit(updatedTodos)
+                }}
+              />
+              <TextField
+                sx={{ flexGrow: 1, marginTop: '1rem', marginRight: '1rem' }}
+                label='What to do?'
+                value={todoToRender.title}
+                onChange={(event) => {
+                  const updatedTodos = todos.map((todo) =>
+                    todoToRender.id === todo.id ? { ...todo, title: event.target.value } : todo
+                  )
+                  handleSubmit(updatedTodos)
+                }}
+              />
+              <TextField
+                type='date'
+                sx={{ flexGrow: 1, marginTop: '1rem' }}
+                value={todoToRender.dueDate ? todoToRender.dueDate.split('T')[0] : ''}
+                onChange={(event) => {
+                  const updatedTodos = todos.map((todo) =>
+                    todoToRender.id === todo.id
+                      ? {
+                          ...todo,
+                          dueDate: new Date(event.target.value).toISOString(),
+                        }
+                      : todo
+                  )
+                  handleSubmit(updatedTodos)
+                }}
+              />
+              <Button
+                sx={{ margin: '8px' }}
+                size='small'
+                color='primary'
+                onClick={() => handleDelete(todoList.id, todoToRender.id)}
+              >
+                <DeleteIcon />
+              </Button>
+            </div>
+          ))}
           <CardActions>
             <Button
               type='button'
